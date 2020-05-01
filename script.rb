52a36d80593cefcf680ad3e7c16beb0e69635888
rajkumardas2701 <rajkumardas2701@gmail.com>
2020-05-01 18:09:54 +0530

    added method my_each_with_index

--- script.rb
+++ script.rb
@@ -2,12 +2,23 @@ module Enumerable
 
     def my_each
         i=0
+        if block_given?
           while i < length
           yield self[i]
           i += 1
+          end
         end
     end
 
-a = ['Raj','Manish']
+    def my_each_with_index
+        i = 0
+        while i < length
+            yield self[i], i
+            i+=1
+        end
+    end
+
+a = ["Raj", "Manish", "Pratima", "Pooja"]
 a.my_each { |friend| puts friend }
+a.my_each_with_index { |friend, index| puts friend if index.odd?}
 end
