1ca20d54af1dc9d126ccc849262bafc3c340cc74
rajkumardas2701 <rajkumardas2701@gmail.com>
2020-05-02 19:19:20 +0000

    added method my_select

--- script.rb
+++ script.rb
@@ -1,24 +1,48 @@
-module Enumerable
+# require 'byebug'
 
+module Enumerable
+# rubocop:disable Style/CaseEquality
     def my_each
-        i=0
+        i = 0
         if block_given?
           while i < length
           yield self[i]
           i += 1
           end
+          return self
         end
+            to_enum
     end
 
     def my_each_with_index
         i = 0
-        while i < length
-            yield self[i], i
-            i+=1
+        if block_given?
+            while i < length
+                yield self[i], i
+                i += 1
+            end
         end
+            to_enum
     end
 
+    def my_select
+        
+        a = []
+        return to_enum unless block_given?
+        
+        my_each {|x| a.push(x) if yield(x)}
+        puts a
+    end
+
+
+        
 a = ["Raj", "Manish", "Pratima", "Pooja"]
+puts "my_each example:"
 a.my_each { |friend| puts friend }
-a.my_each_with_index { |friend, index| puts friend if index.odd?}
+puts "=========================="
+puts "my_each_with_index example:"
+a.my_each_with_index { |friend, index| puts friend if index.even?}
+puts "=========================="
+puts "my_select example:"
+a.my_select {|x| x != "Raj"}
 end
